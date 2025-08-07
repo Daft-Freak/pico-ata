86488b243fe3d2216e74a45afefd9ae324901a9f
Charlie Birks <charlie@daft.games>
2025-08-07 20:11:44 +0100

    Initial commit

---
+++ pico-ata.cpp
@@ -0,0 +1,22 @@
+
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+#include "config.h"
+
+
+int main()
+{
+    //init io
+
+
+    stdio_init_all();
+
+
+    while(true)
+    {
+       
+    }
+
+    return 0;
+}
