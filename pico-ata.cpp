2546ae8a998f76cca12896238193777621e171bc
Charlie Birks <charlie@daft.games>
2025-08-09 15:56:35 +0100

    Add some initial PIO code for... PIO reads
    Already a lot faster, the naming is going to get confusing though

--- pico-ata.cpp
+++ pico-ata.cpp
@@ -1,10 +1,13 @@
 #include <cstdio>
+#include <cmath>
 
+#include "hardware/clocks.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
 #include "tusb.h"
 
+#include "ata.pio.h"
 #include "config.h"
 
 // includes CS and addr
@@ -35,6 +38,9 @@ enum class ATACommand
     IDENTIFY_DEVICE = 0xEC,
 };
 
+static const PIO ata_pio = pio0;
+static int ata_read_pio_sm = -1;
+
 static void init_io()
 {
     // setup all the IO
@@ -47,31 +53,53 @@ static void init_io()
     // also set address pins to output
     mask |= ATA_ADDR_PIN_MASK;
     gpio_set_dir_out_masked(mask);
+
+    // PIO init
+    int read_program_offset = pio_add_program(pio0, &pio_read_program);
+    ata_read_pio_sm = pio_claim_unused_sm(ata_pio, true);
+
+    // setup read pin
+    pio_sm_set_pins_with_mask(ata_pio, ata_read_pio_sm, 0, ATA_READ_PIN_MASK);
+    pio_sm_set_pindirs_with_mask(ata_pio, ata_read_pio_sm, ATA_READ_PIN_MASK, ATA_READ_PIN_MASK);
+    pio_gpio_init(ata_pio, ATA_READ_PIN);
+
+    pio_sm_config c = pio_read_program_get_default_config(read_program_offset);
+
+    sm_config_set_in_shift(&c, false, true, 16); // data
+    sm_config_set_out_shift(&c, false, true, 16); // read count
+
+    sm_config_set_in_pins(&c, ATA_DATA_PIN_BASE);
+    sm_config_set_sideset_pins(&c, ATA_READ_PIN);
+
+    // calc clkdiv
+    double clock_ns = 1000000000.0 / clock_get_hz(clk_sys);
+    int target_ns = 600 / 4; // PIO mode 0 cycle time, 4 instructions for the loop in the program
+    int clkdiv = ceil(target_ns / clock_ns);
+    sm_config_set_clkdiv_int_frac8(&c, clkdiv, 0);
+
+    // init and start
+    pio_sm_init(ata_pio, ata_read_pio_sm, read_program_offset, &c);
+
+    pio_sm_set_enabled(ata_pio, ata_read_pio_sm, true);
 }
 
 static uint16_t read_register(ATAReg reg)
 {
-    // the slowest cycle time we could possibly need here is 660ns
-
     // set address
     gpio_put_masked(ATA_CS_PIN_MASK | ATA_ADDR_PIN_MASK, static_cast<int>(reg) >> 3 << ATA_CS_PIN_BASE | (static_cast<int>(reg) & 7) << ATA_ADDR_PIN_BASE);
 
-    sleep_us(1); // 70ns worst case
+    // count = 1
+    pio_sm_put_blocking(ata_pio, ata_read_pio_sm, 1);
 
-    // assert IOR
-    gpio_put(ATA_READ_PIN, false);
-    sleep_us(1); // 290ns for mode 0-2
-
-    uint16_t data = gpio_get_all() >> ATA_DATA_PIN_BASE;
-
-    gpio_put(ATA_READ_PIN, true);
+    // get result
+    uint16_t data = pio_sm_get_blocking(ata_pio, ata_read_pio_sm);
 
     return data;
 }
 
 static void write_register(ATAReg reg, uint16_t data)
 {
-    // the slowest cycle time we could possibly need here is 660ns
+    // the slowest cycle time we could possibly need here is 600ns
 
     // set address
     gpio_put_masked(ATA_CS_PIN_MASK | ATA_ADDR_PIN_MASK, static_cast<int>(reg) >> 3 << ATA_CS_PIN_BASE | (static_cast<int>(reg) & 7) << ATA_ADDR_PIN_BASE);
